119d9010cff1d55c206e98194c7bf543ff198a7c
Konstantin Helmsauer <konstantin.helmsauer@charite.de>
2019-12-16 10:37:00 +0100

    NA

---
+++ Code/PlotAssemblyMapping.R
@@ -0,0 +1,217 @@
+library(GenomicAlignments)
+library(plyranges)
+library(dplyr)
+library(ggplot2)
+library(data.table)
+source("/Volumes/Elements/MYCNAmplicon/Code/CustomThemes.R")
+
+# ------------------------------------------------------------------------------
+# Plot IMR-5/75 de novo assembly
+# ------------------------------------------------------------------------------
+
+paf = read.table("/Volumes/Elements/IMR-5-75.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.paf") %>% as_tibble()
+colnames(paf) = c(
+  "Query", "QueryLength", "QueryStart", "QueryEnd", "RelativeStrand", 
+  "ReferenceSequence", "ReferenceSequenceLength", "ReferenceSequenceStart", "ReferenceSequenceEnd",
+  "NumberOfResidueMatches", "AlignmentBlockLength", "MappingQuality", 
+  "NoIdea1", "NoIdea2", "NoIdea3", "NoIdea4", "NoIdea5"
+)
+paf = paf %>% filter(Query == "contig_499")
+paf$ReferenceStart = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceStart, paf$ReferenceSequenceEnd)
+paf$ReferenceEnd = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceEnd, paf$ReferenceSequenceStart)
+
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceSequenceStart, yend=ReferenceSequenceEnd)) + 
+  theme_kons2() +
+  xlab("De novo assembly") + 
+  ylab("chromosome 2 (hg19)") +
+  ggtitle("IMR-5/75") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/IMR575_NanoporeDeNovoAssembly_contig499_maptochr2.pdf", 
+       height = 2, width=1.5)
+
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  geom_segment(aes(y=QueryStart, yend=QueryEnd, x=ReferenceStart, xend=ReferenceEnd)) + 
+  theme_kons2() +
+  ylab("De novo assembly") + 
+  xlab("chromosome 2 (hg19)") +
+  ggtitle("Kelly") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/IMR575_NanoporeDeNovoAssembly_contig499_maptochr2_Layout2.pdf", 
+         height = 2, width=1.5)
+
+
+# ------------------------------------------------------------------------------
+# Plot Kelly de novo assembly
+# ------------------------------------------------------------------------------
+
+# samtools view -h -o /Volumes/Elements/Kelly.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.sam /Volumes/Elements/Kelly.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.bam
+# ~/Desktop/nano-wgs/utils/k8-darwin ~/Desktop/nano-wgs/utils/sam2paf.js /Volumes/Elements/Kelly.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.sam > /Volumes/Elements/Kelly.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.paf
+
+paf = read.table("/Volumes/Elements/Kelly.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.paf") %>% as_tibble()
+colnames(paf) = c(
+  "Query", "QueryLength", "QueryStart", "QueryEnd", "RelativeStrand", 
+  "ReferenceSequence", "ReferenceSequenceLength", "ReferenceSequenceStart", "ReferenceSequenceEnd",
+  "NumberOfResidueMatches", "AlignmentBlockLength", "MappingQuality", 
+  "NoIdea1", "NoIdea2", "NoIdea3", "NoIdea4", "NoIdea5"
+)
+paf = paf %>% filter(Query == "contig_31")
+paf$ReferenceStart = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceStart, paf$ReferenceSequenceEnd)
+paf$ReferenceEnd = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceEnd, paf$ReferenceSequenceStart)
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+  theme_kons2() +
+  xlab("De novo assembly") + 
+  ylab("chromosome 2 (hg19)") +
+  ggtitle("Kelly") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/Kelly_NanoporeDeNovoAssembly_contig_31_maptochr2.pdf", 
+         height = 2, width=1.5)
+
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  annotate(ymin=16080683, ymax=16087129, xmin=-Inf, xmax=Inf, geom="rect", color = "steelblue") +
+  annotate(ymin=16375277, ymax=16385344, xmin=-Inf, xmax=Inf, geom="rect", color = "firebrick3") +
+  geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+  theme_kons2() +
+  xlab("De novo assembly") + 
+  ylab("chromosome 2 (hg19)") +
+  ggtitle("Kelly") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/Kelly_NanoporeDeNovoAssembly_contig_31_maptochr2_Annot.pdf", 
+         height = 2, width=1.5)
+  
+paf %>%
+  ggplot() + 
+  geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+  theme_kons2() +
+  xlab("De novo assembly") + 
+  facet_grid(ReferenceSequence ~ .) +
+  ggtitle("Kelly") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/Kelly_NanoporeDeNovoAssembly_contig_31_maptogenome.pdf", 
+         height = 1.5, width=2)
+
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  geom_segment(aes(y=QueryStart, yend=QueryEnd, x=ReferenceStart, xend=ReferenceEnd)) + 
+  theme_kons2() +
+  ylab("De novo assembly") + 
+  xlab("chromosome 2 (hg19)") +
+  ggtitle("Kelly") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/Kelly_NanoporeDeNovoAssembly_contig_31_maptochr2_Layout2.pdf", 
+         height = 2, width=1.5)
+
+# ------------------------------------------------------------------------------
+# Plot CHP-212 de novo assembly
+# ------------------------------------------------------------------------------
+
+paf = fread("/Volumes/Elements/CHP-212.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.paf",
+            fill=T) %>% as_tibble()
+colnames(paf) = c(
+  "Query", "QueryLength", "QueryStart", "QueryEnd", "RelativeStrand", 
+  "ReferenceSequence", "ReferenceSequenceLength", "ReferenceSequenceStart", "ReferenceSequenceEnd",
+  "NumberOfResidueMatches", "AlignmentBlockLength", "MappingQuality", 
+  "NoIdea1", "NoIdea2", "NoIdea3", "NoIdea4", "NoIdea5", "NoIdea6"
+)
+paf = paf %>% filter(Query == "contig_1695")
+paf$ReferenceStart = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceStart, paf$ReferenceSequenceEnd)
+paf$ReferenceEnd = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceEnd, paf$ReferenceSequenceStart)
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+  theme_kons2() +
+  xlab("De novo assembly") + 
+  ylab("chromosome 2 (hg19)") +
+  ggtitle("CHP-212") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/CHP212_NanoporeDeNovoAssembly_contig1695_maptochr2.pdf", 
+         height = 2, width=1.5)
+
+paf %>%
+  filter(ReferenceSequence == "chr2") %>% 
+  ggplot() + 
+  geom_segment(aes(y=QueryStart, yend=QueryEnd, x=ReferenceStart, xend=ReferenceEnd)) + 
+  theme_kons2() +
+  ylab("De novo assembly") + 
+  xlab("chromosome 2 (hg19)") +
+  ggtitle("CHP-212") +
+  ggsave("/Volumes/Elements/MYCNAmplicon/Results/CHP212_NanoporeDeNovoAssembly_contig1695_maptochr2_Layout2.pdf", 
+         height = 2, width=1.5)
+
+ paf %>%
+  ggplot() + 
+  geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+  theme_kons2() +
+  xlab("De novo assembly") + 
+  facet_grid(ReferenceSequence ~ .) +
+  ggtitle("CHP-212")
+
+ # ------------------------------------------------------------------------------
+ # Plot NGP de novo assembly
+ # ------------------------------------------------------------------------------
+ 
+ paf = fread("/Volumes/Elements/NGP.ONT-flye-assembly-meta-genomesize1g/assembly.maptohg19.paf",
+             fill=T) %>% as_tibble()
+ colnames(paf) = c(
+   "Query", "QueryLength", "QueryStart", "QueryEnd", "RelativeStrand", 
+   "ReferenceSequence", "ReferenceSequenceLength", "ReferenceSequenceStart", "ReferenceSequenceEnd",
+   "NumberOfResidueMatches", "AlignmentBlockLength", "MappingQuality", 
+   "NoIdea1", "NoIdea2", "NoIdea3", "NoIdea4", "NoIdea5", "NoIdea6"
+ )
+ paf = paf %>% filter(Query == "contig_45")
+ paf$ReferenceStart = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceStart, paf$ReferenceSequenceEnd)
+ paf$ReferenceEnd = ifelse(paf$RelativeStrand == "+", paf$ReferenceSequenceEnd, paf$ReferenceSequenceStart)
+ 
+ paf %>%
+   filter(ReferenceSequence == "chr2") %>% 
+   ggplot() + 
+   annotate(ymin=16080683, ymax=16087129, xmin=-Inf, xmax=Inf, geom="rect", color = "steelblue") +
+   annotate(ymin=16375277, ymax=16385344, xmin=-Inf, xmax=Inf, geom="rect", color = "firebrick3") +
+   geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+   theme_kons2() +
+   xlab("De novo assembly") + 
+   ylab("chromosome 2 (hg19)") +
+   ggtitle("NGP") +
+   ylim(15000000,18000000) +
+   ggsave("/Volumes/Elements/MYCNAmplicon/Results/NGP_NanoporeDeNovoAssembly_contig45_maptochr2_annotateMYCNAndE4.pdf", 
+          height = 2, width=1.5)
+ 
+ paf %>%
+   filter(ReferenceSequence == "chr2") %>% 
+   ggplot() + 
+   geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+   theme_kons2() +
+   xlab("De novo assembly") + 
+   ylab("chromosome 2 (hg19)") +
+   ggtitle("NGP") +
+   ylim(15000000,18000000) +
+   ggsave("/Volumes/Elements/MYCNAmplicon/Results/NGP_NanoporeDeNovoAssembly_contig45_maptochr2.pdf", 
+          height = 2, width=1.5)
+ 
+ paf %>%
+   filter(ReferenceSequence == "chr2") %>% 
+   ggplot() + 
+   geom_segment(aes(y=QueryStart, yend=QueryEnd, x=ReferenceStart, xend=ReferenceEnd)) + 
+   theme_kons2() +
+   ylab("De novo assembly") + 
+   xlab("chromosome 2 (hg19)") +
+   ggtitle("NGP") +
+   ggsave("/Volumes/Elements/MYCNAmplicon/Results/NGP_NanoporeDeNovoAssembly_contig45_maptochr2_Layout2.pdf", 
+          height = 2, width=1.5)
+ 
+ paf %>%
+   ggplot() + 
+   geom_segment(aes(x=QueryStart, xend=QueryEnd, y=ReferenceStart, yend=ReferenceEnd)) + 
+   theme_kons2() +
+   xlab("De novo assembly") + 
+   facet_grid(ReferenceSequence ~ .) +
+   ggtitle("NGP") +
+   ggsave("/Volumes/Elements/MYCNAmplicon/Results/NGP_NanoporeDeNovoAssembly_contig45_allchr.pdf", 
+          height = 2, width=1.5)
+ 
+ 
+ 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Code accompanying the Helmsauer, Valieva, Ali et al. *Enhancer hijacking determines extrachromosomal MYCN amplicon architecture in neuroblastoma* ([bioRxiv](https://www.henssenlab.com))
+Data analysis for Helmsauer, Valieva, Ali et al. *Enhancer hijacking determines extrachromosomal MYCN amplicon architecture in neuroblastoma* ([bioRxiv](https://www.henssenlab.com))
 
 - `IdentifyCandidateEnhancers.R` integrates RNA-seq and ChIP-seq to identify *MYCN*-driving enhancers in neuroblastoma
 - `DepuydtDataMYCNAmplicon.R` generates aggregate copy number profiles around *MYCN*
@@ -8,5 +8,9 @@ Code accompanying the Helmsauer, Valieva, Ali et al. *Enhancer hijacking determi
 - `MotifScanning.R` scans enhancers for transcription factor binding motifs
 - `ReconstructAmplicon_{CHP212,IMR575}.R` reconstructs the *MYCN* amplicon from structural rearrangement calls for neuroblastoma cell lines and plots the reconstruction together with epigenomic data
 - `FragileSites.R` overlaps aggregate copy number profiles and common fragile sites
+- `ClassIIWGSRearrangementPlotting.R` plots rearrangement data for three patients that lack the e4 enhancer
+- `EnhancerVsCRCChIPseq.R` describes overlap between core regulatory circuit factor ChIP-seq and *MYCN*-driving enhancers
+- `prepareRNAseq.R` parses and normalizes RNA-seq data
+- `PlotAssemblyMapping.R` plots mapping of *de novo* assembled contigs to hg19
 
 Please do not hesitate to contact us at henssenlab@gmail.com.
